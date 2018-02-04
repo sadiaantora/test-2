244d75dd3d16b19330923542a9333165daec13a3
Sadiaantora <sadia.gazi.antora@gmail.com>
2018-02-04 13:05:42 +0000

    First commit added

---
+++ src/Day1.java
@@ -0,0 +1,10 @@
+public class Day1 {
+    public static void main(String[] args) {
+        String greeting = "Hi Hello How are you";
+        System.out.print(greeting);
+        System.out.print(" There");
+        System.out.print("Hello");
+        System.out.print("Sky");
+        int x; //int y;
+    }
+}
